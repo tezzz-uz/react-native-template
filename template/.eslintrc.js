044173604326e7abc0d5815aba55adbaf5453f4f
Nuriddin <nmuhammadjanov@gmail.com>
2025-01-26 02:22:27 +0500

    upgrade react-native to 0.77

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ["@react-native", "plugin:prettier/recommended"],
-}
+  extends: '@react-native',
+};
--- template/.prettierrc.js
+++ template/.prettierrc.js
@@ -1,26 +1,7 @@
-/**
- * @type {import('prettier').Options}
- */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "all",
-  bracketSpacing: true,
+  arrowParens: 'avoid',
   bracketSameLine: true,
-  plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
-  importOrder: [
-    "^react$",
-    "<THIRD_PARTY_MODULES>",
-    "^~(.*)$",
-    "(.*)/screens/",
-    "(.*)/hooks/",
-    "@env",
-    "^../",
-    "^./",
-  ],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-}
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
